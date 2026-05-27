dc4f287c5e1c11fa3cffa429956b77cce083819c
Adith Selvakumar <adithsel@gmail.com>
2026-05-27 10:06:48 -0700

    added third upgrade

--- first_js_file.js
+++ first_js_file.js
@@ -2,6 +2,7 @@ let money = 0
 let clickpower = 1
 let firstupgradebought = false
 let betterbuttonbought = false
+let thirdupgradebought = false
 let mode = 'clicking'
 
 
@@ -30,6 +31,7 @@ document.addEventListener('keydown', event => {
             }
             mode = 'clicking'
             updateDisplay()
+
         } else if (event.key === '2' && !event.repeat) {
             if (money >= 500 && !betterbuttonbought) {
                 clickpower += 3.5
@@ -38,6 +40,18 @@ document.addEventListener('keydown', event => {
                 alert('you bought the better button')
             } else {
                 alert('cannot buy the better button')
+            }
+            mode = 'clicking'
+            updateDisplay()
+
+        } else if (event.key === '3' && !event.repeat) {
+            if (money >= 1250 && !thirdupgradebought) {
+                clickpower += 5.5
+                money -= 1250
+                thirdupgradebought = true
+                alert('you bought the third upgrade')
+            } else {
+                alert('cannot buy the third upgrade')
             }
             mode = 'clicking'
             updateDisplay()
@@ -57,5 +71,6 @@ function buy() {
     mode = 'buying'
     textEl.innerHTML += '<br>1 | first upgrade | $100'
     textEl.innerHTML += '<br>2 | better button | $500'
+    textEl.innerHTML += '<br>3 | third upgrade | $1250'
     textEl.innerHTML += '<br>what will you buy (press associated number): '
 }
