e0854e2e9a2c7fec6e1f71cd67444a7e43c444a6
Adith Selvakumar <adithsel@gmail.com>
2026-05-27 08:51:47 -0700

    initial commit

---
+++ first_js_file.js
@@ -0,0 +1,61 @@
+let money = 0
+let clickpower = 1
+let firstupgradebought = false
+let betterbuttonbought = false
+let mode = 'clicking'
+
+
+
+
+
+const textEl = document.getElementById('text')
+
+document.addEventListener('keydown', event => {
+    if (mode === 'clicking') {
+        if (event.key === 'Enter' && !event.repeat) {
+            money += clickpower
+            updateDisplay()
+        } else if (event.key === 'b' && !event.repeat) {
+            buy()
+        }
+    } else if (mode === 'buying') {
+        if (event.key === '1' && !event.repeat) {
+            if (money >= 100 && !firstupgradebought) {
+                clickpower += 1
+                money -= 100
+                firstupgradebought = true
+                textEl.innerHTML += '<br>you bought the first upgrade'
+            } else {
+                textEl.innerHTML += '<br>cannot buy the first upgrade'
+            }
+            mode = 'clicking'
+            updateDisplay()
+        } else if (event.key === '2' && !event.repeat) {
+            if (money >= 500 && !betterbuttonbought) {
+                clickpower += 3.5
+                money -= 500
+                betterbuttonbought = true
+                textEl.innerHTML += '<br>you bought the better button'
+            } else {
+                textEl.innerHTML += '<br>cannot buy the better button'
+            }
+            mode = 'clicking'
+            updateDisplay()
+        }
+    }
+})
+
+updateDisplay()
+
+
+
+function updateDisplay() {
+    textEl.innerHTML = `<br>type b to buy upgrades and press Enter for money:<br>you have $${money}`
+}
+
+function buy() {
+    mode = 'buying'
+    textEl.innerHTML += '<br>1 | first upgrade | $100'
+    textEl.innerHTML += '<br>2 | better button | $500'
+    textEl.innerHTML += '<br>what will you buy (press associated number): '
+}
